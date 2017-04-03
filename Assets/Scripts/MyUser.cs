f5d34cacb34ba907b2087bada9455f90c57f9cc3
pnwoodsum <pnw8434@ad.rit.edu>
2017-04-03 13:40:53 -0400

    Added some class, EventSystem not working properly

---
+++ Assets/Scripts/MyUser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyUser : MonoBehaviour {
+
+    public GameObject textElement;
+    private Text textBox;
+    public GameObject invObj;
+    public GameObject selected;
+    private Item itemScript;
+    private InventoryManager invScript;
+
+	// Use this for initialization
+	void Start () {
+        selected = null;
+        invScript = invObj.GetComponent<InventoryManager>();
+        textBox = textElement.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/objectInteraction.cs
+++ Assets/Scripts/objectInteraction.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class objectInteraction : MonoBehaviour {
 
+
+    private int mouseClicks = 0;
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        //textBox = textElement.GetComponent<Text>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonUp(0))
         {
+            mouseClicks++;
+            //textBox.text = "Mouse was clicked " + mouseClicks + " times.";
             Ray clickRay = new Ray(Input.mousePosition, new Vector3(0.0f, 0.0f, 1.0f));
             RaycastHit clickRayHit;
             if (Physics.Raycast(clickRay, out clickRayHit, 20))
