7c0a67afcb656bfe06fa4a4b48f2961725342254
pnwoodsum <pnw8434@ad.rit.edu>
2017-04-03 13:42:32 -0400

    Addded some textures and Scripts. Clicking not working

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour, IPointerClickHandler
+{
+    public enum type
+    {
+        CANDLE,
+        DOOR,
+        MATCHES
+    };
+    public type itemType;
+    public GameObject myUserObject;
+    public MyUser myUserScript;
+    public Text userText;
+    public GameObject inventory;
+    protected bool inInventory;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("started");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void init()
+    {
+        inInventory = false;
+        myUserScript = myUserObject.GetComponent<MyUser>();
+        userText = myUserScript.textElement.GetComponent<Text>();
+    }
+    public virtual void OnPointerClick(PointerEventData eventData) { }
+    
+}
